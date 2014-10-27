0931847bf787dd084b894db180bb309918ab6a10
Xue Qian <xueqian0321@126.com>
2014-10-27 21:52:47 +0800

    finish JdbcUtil.java

--- src/main/java/com/thoughtworks/iamcoach/util/JdbcUtil.java
+++ src/main/java/com/thoughtworks/iamcoach/util/JdbcUtil.java
@@ -1,4 +1,38 @@
 package com.thoughtworks.iamcoach.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcUtil {
+
+    private String driverName = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/pos_java";
+    private String userName = "root";
+    private String password = "xueqian";
+
+    private Connection conn;
+
+    public Connection getConnection() {
+
+        try {
+            Class.forName(driverName);
+            conn = DriverManager.getConnection(url, userName, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public void closeConnection(){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
