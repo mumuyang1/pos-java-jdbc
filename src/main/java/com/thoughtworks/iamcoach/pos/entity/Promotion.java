b45977b147daea0e6ddb88537208d558dc8e88b5
Xue Qian <xueqian0321@126.com>
2014-10-27 22:48:59 +0800

    finish Promotion.java

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Promotion.java
@@ -1,4 +1,41 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
 public class Promotion {
+
+    private int id;
+    private int type;
+    private String description;
+
+    public Promotion() {
+    }
+
+    public Promotion(int id, int type, String description) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
