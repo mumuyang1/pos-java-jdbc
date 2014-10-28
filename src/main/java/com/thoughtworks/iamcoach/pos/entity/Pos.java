b524924d1fc6c6b46d66ce669ef19a72bfe6b7d8
Xue Qian <xueqian0321@126.com>
2014-10-28 17:26:00 +0800

    implement Pos.java

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Pos.java
@@ -1,4 +1,42 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
+
 public class Pos {
+
+    private Scanner scanner;
+    private Promotion promotion;
+    private double discount;
+
+    public Pos() {
+    }
+
+    public Pos(Scanner scanner, Promotion promotion, double discount) {
+        this.scanner = scanner;
+        this.promotion = promotion;
+        this.discount = discount;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 }
