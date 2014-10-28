d44fd5cccc14558e8d88258c6c1b47d629b433a6
liyanzi <843624084@qq.com>
2014-10-28 23:26:19 +0800

    override calculate in PromotionOneFree and test

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFree.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFree.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
-public class PromotionOneFree {
+public class PromotionOneFree extends Promotion {
+    @Override
+    public double calculate(CartItem cartItem) {
+        return 0;
+    }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFreeTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFreeTest.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionOneFreeTest {
+    @Test
+    public void should_return_money_with_a_one_free_promotion() {
+        PromotionOneFree promotionOneFree = new PromotionOneFree();
+        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,2),2,"全球美食");
+        assertThat(promotionOneFree.calculate(cartItem)).isEqualTo(0);
+    }
 }
