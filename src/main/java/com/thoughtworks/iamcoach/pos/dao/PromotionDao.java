d2d2fcc75eea980f32fe9bd2d0d8fd79df667e25
liyanzi <843624084@qq.com>
2014-10-28 14:49:09 +0800

    finish PromotionDao

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -1,4 +1,9 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import com.thoughtworks.iamcoach.pos.entity.Promotion;
+
+import java.util.List;
+
 public interface PromotionDao {
+    List<Promotion> getPromotions();
 }
