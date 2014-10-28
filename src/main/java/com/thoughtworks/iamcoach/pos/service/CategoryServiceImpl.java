192af9012bbd1e78d9c472c36d863278b7ea44fe
Xue Qian <xueqian0321@126.com>
2014-10-28 10:20:44 +0800

    create CategoryServiceImpl and implements test

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/CategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos.service;
+
+import com.thoughtworks.iamcoach.pos.entity.Category;
+
+/**
+ * Created by xueqian on 10/28/14.
+ */
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public Category getCategory(int id) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/service/CategoryServiceImplTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CategoryServiceImplTest.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CategoryServiceImplTest {
+
+    @Test
+    public void should_have_getCategory_by_id_function(){
+        CategoryService categoryServiceImpl = new CategoryServiceImpl();
+        assertThat(categoryServiceImpl.getCategory(1)).isEqualTo(null);
+    }
 }
