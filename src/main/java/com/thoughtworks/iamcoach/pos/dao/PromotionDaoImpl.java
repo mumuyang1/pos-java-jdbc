3b5d3bd40d7a4b48ded730a6150ee4d273b6d3a3
Xue Qian <xueqian0321@126.com>
2014-10-28 16:54:57 +0800

    implement PromotionDaoImpl.java

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDaoImpl.java
@@ -1,12 +1,34 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
+import com.thoughtworks.iamcoach.pos.util.JdbcUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PromotionDaoImpl implements PromotionDao {
+    ResultSet rs;
+    PreparedStatement pre;
+    private JdbcUtil jdbcUtil = new JdbcUtil();
+    Connection conn = jdbcUtil.getConnection();
     @Override
-    public Promotion getPromotion() {
-        return null;
+    public Promotion getPromotion(int id) {
+        Promotion promotion = null;
+        try {
+            pre = conn.prepareStatement( "SELECT * FROM categories WHERE id = ?");
+            pre.setInt(1,id);
+            rs = pre.executeQuery();
+            rs.next();
+            promotion = new Promotion(id,rs.getInt("type"),rs.getString("name"));
+            rs.close();
+            pre.close();
+            jdbcUtil.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return promotion;
     }
 
-    
 }
