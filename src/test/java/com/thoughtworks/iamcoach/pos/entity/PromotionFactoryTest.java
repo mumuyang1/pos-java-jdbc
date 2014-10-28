5bebbe49c4806a56f2e6734aa3c42668b0e88076
liyanzi <843624084@qq.com>
2014-10-28 20:49:54 +0800

    add PromotionFactory class and test

---
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionFactoryTest.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.iamcoach.pos.entity;
+
+import org.junit.Test;
+
+public class PromotionFactoryTest {
+    @Test
+    public void should_return_promoion_by_type(){
+        PromotionFactory promotionFactory = new PromotionFactory();
+    }
+
+
+}
