4f5d5873b2406733c7333c0a973cb544eb98a8fa
liyanzi <843624084@qq.com>
2014-10-28 12:03:55 +0800

    finish FileProcessor and test pass

--- src/main/java/com/thoughtworks/iamcoach/pos/util/FileProcessor.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/FileProcessor.java
@@ -1,4 +1,27 @@
 package com.thoughtworks.iamcoach.pos.util;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileProcessor {
+
+    public static List readFile(String fileName) {
+
+        String filePath = FileProcessor.class.getClassLoader().getResource(fileName).getPath();
+        List<String> data = new ArrayList<String>();
+        try {
+
+            data = Files.readAllLines(Paths.get(filePath),
+                    Charset.defaultCharset());
+        } catch (IOException e) {
+
+            System.out.print("读取文件失败。。。。。。");
+        }
+        return data;
+    }
+
 }
---
+++ src/test/java/com/thoughtworks/iamcoach/pos/util/FileProcessorTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.iamcoach.pos.util;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class FileProcessorTest {
+
+    @Test
+    public void should_get_a_list() {
+
+        FileProcessor fileProcessor = mock(FileProcessor.class);
+
+        String fileName = "cart.txt";
+        assertThat(fileProcessor.readFile(fileName).get(0)).isEqualTo("ITEM000001");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void should_throws_exception()throws NullPointerException{
+        FileProcessor.readFile("i");
+    }
+
+}
