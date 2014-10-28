d5533514a5132fa7b45a1204d15d562024ce0388
liyanzi <843624084@qq.com>
2014-10-28 12:50:12 +0800

    add getItems method and test

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Scanner.java
@@ -1,5 +1,12 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
 
+    public List<CartItem> getCartItems() {
+        List<CartItem> cartItems = new ArrayList<CartItem>();
+        return null;
+    }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/entity/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/ScannerTest.java
@@ -1,6 +1,16 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ScannerTest {
 
-  
+    @Test
+    public void should_return_a_cart_list() {
+
+        Scanner cartItemProcessor = new Scanner();
+        assertThat(cartItemProcessor.getCartItems()).isEqualTo(null);
+
+    }
 }
