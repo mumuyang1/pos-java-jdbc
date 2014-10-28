122dd07408f301d0a2e1c6848efc9cd28023ec63
Xue Qian <xueqian0321@126.com>
2014-10-28 16:57:53 +0800

    implement interface PromotionService

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -1,4 +1,7 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.entity.Promotion;
+
 public interface PromotionService {
+    Promotion getPromotion(int id);
 }
