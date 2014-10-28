d4344d26508ec8acaa15a8917286c181dea827ec
Xue Qian <xueqian0321@126.com>
2014-10-28 09:46:11 +0800

    finish getItem at service and test pass

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImpl.java
@@ -1,14 +1,17 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.dao.ItemDao;
+import com.thoughtworks.iamcoach.pos.dao.ItemDaoImpl;
 import com.thoughtworks.iamcoach.pos.entity.Item;
 
 import java.util.List;
 
 public class ItemServiceImpl implements ItemService {
+    private ItemDao itemDaoImpl = new ItemDaoImpl();
 
     @Override
     public Item getItem(String barcode) {
-        return null;
+        return itemDaoImpl.getItem(barcode);
     }
 
     @Override
--- src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImplTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImplTest.java
@@ -7,10 +7,39 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class ItemServiceImplTest {
 
     @Test
-    public void should_have_getItem_function(){
+    public void should_return_correct_getName_by_getItem_function(){
         ItemService itemServiceImpl = new ItemServiceImpl();
-        assertThat(itemServiceImpl.getItem("ITEM000001")).isEqualTo(null);
+        assertThat(itemServiceImpl.getItem("ITEM000001").getName()).isEqualTo("女装");
+    }
 
+    @Test
+    public void should_return_correct_getBarcode_by_getItem_function(){
+        ItemService itemServiceImpl = new ItemServiceImpl();
+        assertThat(itemServiceImpl.getItem("ITEM000002").getBarcode()).isEqualTo("ITEM000002");
+    }
+
+    @Test
+    public void should_return_correct_getId_by_getItem_function(){
+        ItemService itemServiceImpl = new ItemServiceImpl();
+        assertThat(itemServiceImpl.getItem("ITEM000003").getId()).isEqualTo(3);
+    }
+
+    @Test
+    public void should_return_correct_getCategoryId_by_getItem_function(){
+        ItemService itemServiceImpl = new ItemServiceImpl();
+        assertThat(itemServiceImpl.getItem("ITEM000004").getCategoryId()).isEqualTo(2);
+    }
+
+    @Test
+    public void should_return_correct_getPrice_by_getItem_function(){
+        ItemService itemServiceImpl = new ItemServiceImpl();
+        assertThat(itemServiceImpl.getItem("ITEM000005").getPrice()).isEqualTo(30);
+    }
+
+    @Test
+    public void should_return_correct_getUnit_by_getItem_function(){
+        ItemService itemServiceImpl = new ItemServiceImpl();
+        assertThat(itemServiceImpl.getItem("ITEM000006").getUnit()).isEqualTo("个");
     }
 
 }
