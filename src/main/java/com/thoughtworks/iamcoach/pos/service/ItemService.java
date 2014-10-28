554e3b1cb62fa24c40bdb4c08088e6405702016f
Xue Qian <xueqian0321@126.com>
2014-10-28 09:22:46 +0800

    finish interface ItenService

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,4 +1,12 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.entity.Item;
+
+import java.util.List;
+
 public interface ItemService {
+
+    Item getItem(String barcode);
+
+    List<Item> getItems();
 }
