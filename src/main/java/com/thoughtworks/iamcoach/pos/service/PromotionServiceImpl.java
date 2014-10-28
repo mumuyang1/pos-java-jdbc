d13f243ab33ff1ab89e1ffe294064385f9d4f3a0
Xue Qian <xueqian0321@126.com>
2014-10-28 17:03:19 +0800

    create class PromotionServiceImpl and test

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos.service;
+
+import com.thoughtworks.iamcoach.pos.entity.Promotion;
+
+/**
+ * Created by xueqian on 10/28/14.
+ */
+public class PromotionServiceImpl implements PromotionService {
+    @Override
+    public Promotion getPromotion(int id) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImplTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImplTest.java
@@ -1,4 +1,16 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionServiceImplTest {
+
+    @Test
+    public void should_have_getPromotion_by_id_function(){
+        PromotionService promotionServiceImpl = new PromotionServiceImpl();
+        assertThat(promotionServiceImpl.getPromotion(1)).isEqualTo(null);
+
+    }
+
 }
