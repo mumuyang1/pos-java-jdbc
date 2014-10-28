67c266c28bd76c72a4570240568b58d7e7ec620d
Xue Qian <xueqian0321@126.com>
2014-10-28 08:31:59 +0800

    implement getItem by barcode at ItemDaoImpl.java

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDaoImpl.java
@@ -1,4 +1,40 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
-public class ItemDaoImpl {
+import com.thoughtworks.iamcoach.pos.entity.Item;
+import com.thoughtworks.iamcoach.pos.util.JdbcUtil;
+
+import java.sql.*;
+import java.util.List;
+
+
+public class ItemDaoImpl implements ItemDao{
+    ResultSet rs;
+    PreparedStatement pre;
+    private JdbcUtil jdbcUtil = new JdbcUtil();
+    Connection conn = jdbcUtil.getConnection();
+
+    @Override
+    public Item getItem(String barcode) {
+        Item item = null;
+        try {
+            pre = conn.prepareStatement( "SELECT * FROM items WHERE barcode = ?");
+            pre.setString(1, barcode);
+            rs = pre.executeQuery();
+            rs.next();
+            item = new Item(rs.getInt("id"),barcode, rs.getString("name"), rs.getString("unit"), rs.getDouble("price"),rs.getInt("categoryid"));
+            rs.close();
+            pre.close();
+            jdbcUtil.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+
+
 }
