aeb9480d358e82c87df1b3f40485bbf3be55b050
Xue Qian <xueqian0321@126.com>
2014-10-28 09:20:17 +0800

    rename ItemServiceTest to ItemServiceTestImpl

---
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos.service;
+
+import org.junit.Test;
+
+public class ItemServiceImplTest {
+
+    @Test
+    public void should_have_getItem_function(){
+
+
+    }
+
+}
