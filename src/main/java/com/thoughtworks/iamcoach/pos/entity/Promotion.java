6b8cec04262c84a648e94f1b5d32fc356a2c3f1a
Xue Qian <xueqian0321@126.com>
2014-10-28 16:36:01 +0800

    modify Promotion.java

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Promotion.java
@@ -5,14 +5,24 @@ public class Promotion {
     private int id;
     private int type;
     private String description;
+    private double discount;
 
     public Promotion() {
     }
 
-    public Promotion(int id, int type, String description) {
+    public Promotion(int id, int type, String description,double discount) {
         this.id = id;
         this.type = type;
         this.description = description;
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
     }
 
     public int getId() {
