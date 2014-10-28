78cc2e34ceb9b2b0707abf1fe94b3fa77da5af18
Xue Qian <xueqian0321@126.com>
2014-10-28 10:07:38 +0800

    finish CategoryImpl.java

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryImpl.java
@@ -1,10 +1,30 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.entity.Category;
+import com.thoughtworks.iamcoach.pos.util.JdbcUtil;
+
+import java.sql.*;
 
 public class CategoryImpl implements CategoryDao {
+    ResultSet rs;
+    PreparedStatement pre;
+    private JdbcUtil jdbcUtil = new JdbcUtil();
+    Connection conn = jdbcUtil.getConnection();
     @Override
     public Category getCategory(int id) {
-        return null;
+        Category category = null;
+        try {
+            pre = conn.prepareStatement( "SELECT * FROM categories WHERE id = ?");
+            pre.setInt(1,id);
+            rs = pre.executeQuery();
+            rs.next();
+            category = new Category(id,rs.getString("name"));
+            rs.close();
+            pre.close();
+            jdbcUtil.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
     }
 }
