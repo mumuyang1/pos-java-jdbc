24b100e3064dee5ce22d7f58f44f40eef59f40fb
Xue Qian <xueqian0321@126.com>
2014-10-28 10:00:04 +0800

    finish interface CategoryDao and implements class CategoryImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import com.thoughtworks.iamcoach.pos.entity.Category;
+
 public interface CategoryDao {
+
+    Category getCategory(int id);
 }
---
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryImpl.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.pos.dao;
+
+import com.thoughtworks.iamcoach.pos.entity.Category;
+
+public class CategoryImpl implements CategoryDao {
+    @Override
+    public Category getCategory(int id) {
+        return null;
+    }
+}
