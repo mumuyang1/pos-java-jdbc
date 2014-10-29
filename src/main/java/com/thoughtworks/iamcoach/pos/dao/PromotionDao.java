913d32278c0092633ca21f9876813442a139526e
Xue Qian <xueqian0321@126.com>
2014-10-29 17:06:52 +0800

    modify PromotionDao

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -2,8 +2,10 @@ package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
 
+import java.util.Set;
+
 public interface PromotionDao {
     Promotion getPromotion(int id);
 
-    java.util.Set<String> getPromotionBarcode();
+    Set<String> getPromotionBarcode();
 }
