cc20da5e027f45166e35a199c650ad9cc4771998
liyanzi <843624084@qq.com>
2014-10-29 21:43:16 +0800

    finish calculate method and test pass in PromotionOneFree

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFree.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionOneFree.java
@@ -3,6 +3,7 @@ package com.thoughtworks.iamcoach.pos.entity;
 public class PromotionOneFree extends Promotion {
     @Override
     public double calculate(CartItem cartItem) {
-        return 0;
+
+        return (cartItem.getCount() - (int) (cartItem.getCount() / 3)) * cartItem.getItem().getPrice();
     }
 }
