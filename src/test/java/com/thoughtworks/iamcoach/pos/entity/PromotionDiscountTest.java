f85dcdcd579b8b31d89f6da3908fdba75c56dfe9
Xue Qian <xueqian0321@126.com>
2014-10-29 17:57:26 +0800

    refactor PromotionDiscountTest

--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
@@ -8,7 +8,7 @@ public class PromotionDiscountTest {
     @Test
     public void should_return_money_with_a_discount_promotion() {
         PromotionDiscount promotionDiscount = new PromotionDiscount();
-        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,2),2,"全球美食");
+        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,"全球美食"),2,"全球美食");
         assertThat(promotionDiscount.calculate(cartItem)).isEqualTo(0);
     }
 }
