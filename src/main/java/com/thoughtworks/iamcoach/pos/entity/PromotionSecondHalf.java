5d7858819a4babaa3fc89328943e08e60bad09ab
liyanzi <843624084@qq.com>
2014-10-29 21:51:19 +0800

    finish calculate method and test pass in PromotionSecondHalf

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalf.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalf.java
@@ -3,6 +3,6 @@ package com.thoughtworks.iamcoach.pos.entity;
 public class PromotionSecondHalf extends Promotion{
     @Override
     public double calculate(CartItem cartItem) {
-        return 0;
+        return cartItem.getCount() * cartItem.getItem().getPrice() - (int) (cartItem.getCount() / 3) * cartItem.getItem().getPrice()/2;
     }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalfTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalfTest.java
@@ -9,8 +9,7 @@ public class PromotionSecondHalfTest {
     @Test
     public void should_return_money_with_a_second_half_money_promotion() {
         PromotionSecondHalf promotionSecondHalf = new PromotionSecondHalf();
-        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,"全球美食"),2);
-        assertThat(promotionSecondHalf.calculate(cartItem)).isEqualTo(0);
+        CartItem cartItem = new CartItem(new Item(1, "ITEM000001", "麦斯威尔", "盒", 17, "全球美食"), 5);
+        assertThat(promotionSecondHalf.calculate(cartItem)).isEqualTo(76.5);
     }
-
 }
