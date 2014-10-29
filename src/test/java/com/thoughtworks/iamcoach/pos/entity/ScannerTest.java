a65773f6976fa6decbb34d88b712d700c327ecbe
liyanzi <843624084@qq.com>
2014-10-29 20:23:02 +0800

    finish getCartItems and test pass

--- src/test/java/com/thoughtworks/iamcoach/pos/entity/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/ScannerTest.java
@@ -1,16 +1,14 @@
-//package com.thoughtworks.iamcoach.pos.entity;
-//
-//import org.junit.Test;
-//
-//import static org.fest.assertions.api.Assertions.assertThat;
-//
-//public class ScannerTest {
-//
-//    @Test
-//    public void should_return_a_cart_list() {
-//
-//        Scanner cartItemProcessor = new Scanner();
-//        assertThat(cartItemProcessor.getCartItems().get(0).getItem().getName()).isEqualTo("五花肉");
-//
-//    }
-//}
+package com.thoughtworks.iamcoach.pos.entity;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class ScannerTest {
+
+    @Test
+    public void should_return_a_cart_list() {
+        Scanner cartItemProcessor = new Scanner();
+        assertThat(cartItemProcessor.getCartItems().get(0).getItem().getName()).isEqualTo("五花肉");
+    }
+}
