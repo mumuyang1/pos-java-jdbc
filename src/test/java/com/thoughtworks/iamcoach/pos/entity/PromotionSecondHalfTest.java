c1e3dd4d97f69352460252443a083b953cc21176
Xue Qian <xueqian0321@126.com>
2014-10-29 17:58:30 +0800

    refactor PromotionSecondHalf

--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalfTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionSecondHalfTest.java
@@ -9,7 +9,7 @@ public class PromotionSecondHalfTest {
     @Test
     public void should_return_money_with_a_second_half_money_promotion() {
         PromotionSecondHalf promotionSecondHalf = new PromotionSecondHalf();
-        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,2),2,"全球美食");
+        CartItem cartItem = new CartItem(new Item(1,"ITEM000001","麦斯威尔","盒",17,"全球美食"),2,"全球美食");
         assertThat(promotionSecondHalf.calculate(cartItem)).isEqualTo(0);
     }
 
