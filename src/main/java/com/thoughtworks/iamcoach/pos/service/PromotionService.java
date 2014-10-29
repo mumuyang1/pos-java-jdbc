b0e2ac5b8c4776e05f452853a599698ff66c42fd
Xue Qian <xueqian0321@126.com>
2014-10-29 17:09:27 +0800

    finish PromotionService

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -2,6 +2,10 @@ package com.thoughtworks.iamcoach.pos.service;
 
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
 
+import java.util.Set;
+
 public interface PromotionService {
     Promotion getPromotion(int id);
+
+    Set<String> getPromotionBarcode();
 }
