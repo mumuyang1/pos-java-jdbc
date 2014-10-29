6c5aa64229f85f700cc4822432dae6ad628afac5
Xue Qian <xueqian0321@126.com>
2014-10-29 17:55:16 +0800

    refactor PromotionServiceImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImpl.java
@@ -4,11 +4,18 @@ import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
 import com.thoughtworks.iamcoach.pos.dao.PromotionDaoImpl;
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
 
+import java.util.Set;
+
 public class PromotionServiceImpl implements PromotionService {
 
     private PromotionDao promotionDaoImpl = new PromotionDaoImpl();
     @Override
     public Promotion getPromotion(int id) {
         return promotionDaoImpl.getPromotion(id);
+    }
+
+    @Override
+    public Set<String> getPromotionBarcode() {
+        return promotionDaoImpl.getPromotionBarcode();
     }
 }
