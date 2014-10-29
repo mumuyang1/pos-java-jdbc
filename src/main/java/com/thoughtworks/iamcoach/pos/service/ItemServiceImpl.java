30d6d265f330e2b902120e840d1ec90fc94432ae
Xue Qian <xueqian0321@126.com>
2014-10-29 18:25:09 +0800

    refactor ItemServiceImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemServiceImpl.java
@@ -2,14 +2,26 @@ package com.thoughtworks.iamcoach.pos.service;
 
 import com.thoughtworks.iamcoach.pos.dao.ItemDao;
 import com.thoughtworks.iamcoach.pos.dao.ItemDaoImpl;
+import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
+import com.thoughtworks.iamcoach.pos.dao.PromotionDaoImpl;
 import com.thoughtworks.iamcoach.pos.entity.Item;
 
 public class ItemServiceImpl implements ItemService {
-    private ItemDao itemDaoImpl = new ItemDaoImpl();
+
 
     @Override
     public Item getItem(String barcode) {
-        return itemDaoImpl.getItem(barcode);
+        Item item;
+        PromotionDao promotionDaoImpl = new PromotionDaoImpl();
+        if (promotionDaoImpl.getPromotionBarcode().contains(barcode)) {
+            ItemDao itemDaoImpl = new ItemDaoImpl();
+            item = itemDaoImpl.getItem(barcode);
+            itemDaoImpl = new ItemDaoImpl();
+            item.setPromotions(itemDaoImpl.getItemPromotions(barcode));
+            return item;
+        }
+        ItemDao itemDaoImpl = new ItemDaoImpl();
+        item = itemDaoImpl.getItem(barcode);
+        return item;
     }
-
 }
