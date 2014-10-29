9eea7244ec46700e58013116a255abcbf5853e71
Xue Qian <xueqian0321@126.com>
2014-10-29 22:55:19 +0800

    modify PromotionDiscountTest

--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
@@ -1,8 +1,9 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
-import com.thoughtworks.iamcoach.pos.service.ItemService;
-import com.thoughtworks.iamcoach.pos.service.ItemServiceImpl;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -10,9 +11,16 @@ public class PromotionDiscountTest {
     @Test
     public void should_return_money_with_a_discount_promotion() {
         PromotionDiscount promotionDiscount = new PromotionDiscount();
-        ItemService itemServiceImpl = new ItemServiceImpl();
-        Item item = itemServiceImpl.getItem("ITEM000005");
-        CartItem cartItem = new CartItem(item, 5);
-        assertThat(promotionDiscount.calculate(cartItem)).isEqualTo(127.5);
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        Promotion promotion = PromotionFactory.getPromotionByType(1);
+        promotion.setId(1);
+        promotion.setType(1);
+        promotion.setDiscount(90);
+        promotion.setDescription("打折");
+        promotions.add(0, promotion);
+        Item item = new Item (1,"ITEM000001","测试１","你猜",100,"不告诉你");
+        item.setPromotions(promotions);
+        CartItem cartItem = new CartItem(item,2);
+        assertThat(promotionDiscount.calculate(cartItem)).isEqualTo(180);
     }
 }
