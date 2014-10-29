c616c50d8eff91cb87d9fbbf2d7828b8a3e1325e
Xue Qian <xueqian0321@126.com>
2014-10-29 18:49:51 +0800

    refactor CartItem.java

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/CartItem.java
@@ -3,13 +3,6 @@ package com.thoughtworks.iamcoach.pos.entity;
 public class CartItem {
     private Item item;
     private double count;
-    private String category;
-
-    public CartItem(Item item, double count, String category) {
-        this.item = item;
-        this.count = count;
-        this.category = category;
-    }
 
     public CartItem(Item item, double count) {
         this.item = item;
@@ -21,25 +14,16 @@ public class CartItem {
         return item;
     }
 
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
     public double getCount() {
         return count;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    @Override
-    public String toString() {
-        return "CartItem{" +
-                "item=" + item +
-                ", count=" + count +
-                ", category='" + category + '\'' +
-                '}';
+    public void setCount(double count) {
+        this.count = count;
     }
 }
 
