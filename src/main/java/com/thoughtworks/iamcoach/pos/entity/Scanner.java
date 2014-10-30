25fa3edf870427317f43099aca669c86d2540c20
Xue Qian <xueqian0321@126.com>
2014-10-30 21:57:49 +0800

    delete unuseful codes at ItemService.java

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Scanner.java
@@ -1,4 +1,5 @@
 package com.thoughtworks.iamcoach.pos.entity;
+
 import com.thoughtworks.iamcoach.pos.service.ItemService;
 import com.thoughtworks.iamcoach.pos.service.impl.ItemServiceImpl;
 import com.thoughtworks.iamcoach.pos.util.FileProcessor;
--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -2,8 +2,6 @@ package com.thoughtworks.iamcoach.pos.service;
 
 import com.thoughtworks.iamcoach.pos.entity.Item;
 
-import java.util.List;
-
 public interface ItemService {
 
     Item getItem(String barcode);
