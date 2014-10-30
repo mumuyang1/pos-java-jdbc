0e943b7967fe6847b907557cd1c10fbee265b7ce
liyanzi <843624084@qq.com>
2014-10-30 19:54:50 +0800

    modify transfer method to static

--- src/main/java/com/thoughtworks/iamcoach/pos/util/DataTransfer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/DataTransfer.java
@@ -3,7 +3,7 @@ package com.thoughtworks.iamcoach.pos.util;
 import java.math.BigDecimal;
 
 public class DataTransfer {
-    public double transfer(double data) {
+    public static double transfer(double data) {
 
         BigDecimal b = new BigDecimal(data);
         return b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
