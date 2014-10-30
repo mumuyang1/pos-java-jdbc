b73f31b70d93cd400202124f8741091db43907dd
Xue Qian <xueqian0321@126.com>
2014-10-30 19:48:31 +0800

    add DataTransferTest

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/DataTransfer.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.iamcoach.pos.util;
+
+/**
+ * Created by xueqian on 10/30/14.
+ */
+public class DataTransfer {
+}
---
+++ src/test/java/com/thoughtworks/iamcoach/pos/util/DataTransferTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.iamcoach.pos.util;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class DataTransferTest {
+
+    @Test
+    public void should_transfer_data(){
+        DataTransfer dataTransfer = new DataTransfer();
+        assertThat(dataTransfer.transfer(3.4543)).isEqualTo(3.45);
+        assertThat(dataTransfer.transfer(3.458)).isEqualTo(3.46);
+    }
+}
