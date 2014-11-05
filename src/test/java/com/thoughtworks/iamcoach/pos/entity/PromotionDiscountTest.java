55fe00bcd09514193ad12f586396f1640888244f
Xue Qian <xueqian0321@126.com>
2014-11-05 22:27:41 +0800

    modify PromotionDiscountTest

--- src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/entity/PromotionDiscountTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.iamcoach.pos.entity;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,19 +9,37 @@ import java.util.List;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class PromotionDiscountTest {
-    @Test
-    public void should_return_money_with_a_discount_promotion() {
-        PromotionDiscount promotionDiscount = new PromotionDiscount();
-        List<Promotion> promotions = new ArrayList<Promotion>();
-        Promotion promotion = PromotionFactory.getPromotionByType(1);
+    private List<Promotion> promotions;
+    private PromotionDiscount promotionDiscount;
+    private Promotion promotion;
+    private Item item;
+
+    @Before
+    public void before() {
+        promotionDiscount = new PromotionDiscount();
+        promotions = new ArrayList<Promotion>();
+        promotion = PromotionFactory.getPromotionByType(1);
         promotion.setId(1);
         promotion.setType(1);
-        promotion.setDiscount(90);
+        item = new Item(1, "ITEM000001", "测试１", "你猜", 100, "不告诉你");
         promotion.setDescription("打折");
+    }
+
+    @Test
+    public void should_return_money_with_a_discount_promotion() {
+        promotion.setDiscount(90);
         promotions.add(0, promotion);
-        Item item = new Item(1, "ITEM000001", "测试１", "你猜", 100, "不告诉你");
         item.setPromotions(promotions);
         CartItem cartItem = new CartItem(item, 2);
         assertThat(promotionDiscount.calculate(cartItem)).isEqualTo(180);
+    }
+
+    @Test
+    public void should_return_money_without_a_discount_promotion() {
+        promotion.setDiscount(0);
+        promotions.add(0, promotion);
+        item.setPromotions(promotions);
+        CartItem cartItem = new CartItem(item, 2);
+        assertThat(promotionDiscount.calculate(cartItem)).isEqualTo(200);
     }
 }
